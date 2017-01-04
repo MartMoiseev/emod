8e09e791d5034c9df7039641dd2986f5c25d190c
MartMoiseev <moiseev@mart.ru>
2017-01-04 11:57:53 +0300

    Добавил мультитекстурный блок Добавил анимацию для одной из сторон блока Todo: сделать поворот как в печке

--- main/java/ru/emod/EBlock.java
+++ main/java/ru/emod/EBlock.java
@@ -2,11 +2,14 @@ package ru.emod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.IIcon;
 
 public class EBlock extends Block {
 
 	public static final String NAME = "eblock";
+	public IIcon[] icons = new IIcon[6];
 
 	protected EBlock() {
 		super(Material.rock);
@@ -18,4 +21,15 @@ public class EBlock extends Block {
 		this.setBlockTextureName(EMod.MODID + ":" + NAME);
 	}
 
+	@Override
+	public void registerBlockIcons(IIconRegister reg) {
+		for (int i = 0; i < 6; i ++) {
+			this.icons[i] = reg.registerIcon(this.textureName + i);
+		}
+	}
+
+	@Override
+	public IIcon getIcon(int side, int meta) {
+		return this.icons[side];
+	}
 }
