6bc435d6ad8bb7f99027aee2e9898da045a2e65e
MartMoiseev <moiseev@mart.ru>
2017-01-07 14:52:56 +0300

    Добавил простой поворот мультитекстурного блока по 4 сторонам через мета-данные

--- main/java/ru/emod/EBlock.java
+++ main/java/ru/emod/EBlock.java
@@ -4,32 +4,57 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.World;
+import scala.Console;
 
 public class EBlock extends Block {
 
-	public static final String NAME = "eblock";
-	public IIcon[] icons = new IIcon[6];
+    public static final String NAME = "eblock";
+    public static final int SIZE = 6;
+    public IIcon[] icons = new IIcon[SIZE];
 
-	protected EBlock() {
-		super(Material.rock);
-		this.setBlockName(NAME);
-		this.setCreativeTab(CreativeTabs.tabBlock);
-		this.setHardness(15F);
-		this.setResistance(10F);
-		this.setHarvestLevel("pickaxe", 1);
-		this.setBlockTextureName(EMod.MODID + ":" + NAME);
-	}
+    protected EBlock() {
+        super(Material.rock);
+        this.setBlockName(NAME);
+        this.setCreativeTab(CreativeTabs.tabBlock);
+        this.setHardness(15F);
+        this.setResistance(10F);
+        this.setHarvestLevel("pickaxe", 1);
+        this.setBlockTextureName(EMod.MODID + ":" + NAME);
+    }
 
-	@Override
-	public void registerBlockIcons(IIconRegister reg) {
-		for (int i = 0; i < 6; i ++) {
-			this.icons[i] = reg.registerIcon(this.textureName + i);
-		}
-	}
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        for (int i = 0; i < SIZE; i++) {
+            this.icons[i] = reg.registerIcon(this.textureName + i);
+        }
+    }
 
-	@Override
-	public IIcon getIcon(int side, int meta) {
-		return this.icons[side];
-	}
+    @Override
+    public IIcon getIcon(int side, int meta) {
+        // render top and bottom texture
+        if (side < 2) {
+            return this.icons[side];
+        }
+
+        // rotate other texture
+        int[][] array = {
+                {2, 3, 4, 5},
+                {4, 5, 3, 2},
+                {3, 2, 5, 4},
+                {5, 4, 2, 3},
+        };
+
+        return this.icons[array[meta][side - 2]];
+    }
+
+    public void onBlockPlacedBy(World par1World, int par2, int par3, int par4, EntityLivingBase par5EntityLivingBase, ItemStack par6ItemStack) {
+        // get meta [0..4] on rotate
+        int l = MathHelper.floor_double((double) (par5EntityLivingBase.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3;
+        par1World.setBlockMetadataWithNotify(par2, par3, par4, l, 2);
+    }
 }
